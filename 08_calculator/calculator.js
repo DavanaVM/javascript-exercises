f7da9b777af4960172e0aec5e9ced6c45abdf179
DavanaVM <gotfriddavid@gmx.de>
2022-12-12 20:36:08 +0100

    07 and 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(...num) {
+	sol = 0;
+  for(let i = 0; i < num.length; i++){
+    sol += num[i];
+  }
+  return sol
+};
+const subtract = function(num, num1) {
+  return num - num1
+};
+const sum = function(num) {
+	sol = 0;
+  for(let i = 0; i < num.length; i++){
+    sol += num[i];
+  }
+  return sol
 };
 
-const subtract = function() {
-	
+
+const multiply = function(num) {
+	sol = num[0];
+  for(let i = 1; i < num.length; i++){
+    sol *= num[i];
+  }
+  return sol
 };
 
-const sum = function() {
-	
+const power = function(num, power) {
+	sol = num;
+
+  for(i = 1; i < power; i++){
+    sol = sol * num;
+  }
+  return sol
 };
 
-const multiply = function() {
+const factorial = function(fact) {
+  sol = 1
 
-};
+  for(i = 1; i <= fact; i++){
+    sol = sol * i;
+  }
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+	return sol
 };
 
 // Do not edit below this line
