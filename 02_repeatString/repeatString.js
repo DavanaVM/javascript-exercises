4f31e551e344e8ba9bdfcffc945dd042521802c9
DavanaVM <gotfriddavid@gmx.de>
2022-12-12 14:28:59 +0100

    First two excercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    temp = string;
+    if(num < 0){
+        return "ERROR"
+    }else{
+        string = "";
+        for(let i = 1; i<= num; i++){
+            string += temp;
+        }
+        return string
+    }
 };
 
 // Do not edit below this line
