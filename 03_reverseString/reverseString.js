36a4b5d1373a42154745fb40f783e5f3a98f6bc8
DavanaVM <gotfriddavid@gmx.de>
2022-12-12 15:10:27 +0100

    Another 2 tasks

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let temp = "";
+    for(let i = 0; i < string.length; i++){
+        console.log(string.charAt(i))
+        temp = string.charAt(i) + temp;  
+    }
+    return temp;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...number) {
+    for(let i = 0; i < number.length ; i++){
+    console.log(number[i])
+    index = array.indexOf(number[i])
+    if(index == -1){
 
+    }else{
+    array.splice(index, 1)
+    }   
+}
+    return array;
 };
-
 // Do not edit below this line
 module.exports = removeFromArray;
