0ff53242d0f684a1bdd9ab94e9dc826497c09069
DavanaVM <gotfriddavid@gmx.de>
2022-12-12 15:34:50 +0100

    05 and 06 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    let sol = number2
+    if(typeof(number1) != typeof(number2) || number1 < 0 || number2 < 0){
+        return "ERROR"
+    }
+    if(number1 > number2){
+        number2 = number1;
+        number1 = sol;
+    }
+    sol = 0;
+    for(number1 ;number1 <=number2 ; number1++){
+        sol += number1
+    }
+    
+    return sol;
 };
 
 // Do not edit below this line
