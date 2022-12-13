6b64892e46c09e7794975263e8b56488323b9168
DavanaVM <gotfriddavid@gmx.de>
2022-12-13 11:20:04 +0100

    palindromes were easy but fibonacci was tricky

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,32 @@
-const palindromes = function () {
-
+const reverseString = function(string) {
+    let temp = "";
+    for(let i = 0; i < string.length; i++){
+        temp = string.charAt(i) + temp;  
+    }
+    return temp;
 };
 
+const palindromes = function (prompt) {
+    prompt = prompt.toLowerCase()
+    for(let i = 0; i < prompt.length; i++){
+        prompt = prompt.replace(' ', '')
+        prompt = prompt.replace('!', '')
+        prompt = prompt.replace('?', '')
+        prompt = prompt.replace(',', '')
+        prompt = prompt.replace('.', '')
+        prompt = prompt.replace('-', '')
+        prompt = prompt.replace(';', '')
+        prompt = prompt.replace(':', '')
+        prompt = prompt.replace('_', '')
+    }
+    prompt2 = reverseString(prompt)
+    if(prompt2 == prompt){
+        return true
+    }
+    return false
+};
+
+console
 // Do not edit below this line
+
 module.exports = palindromes;
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let i = 0;
+    fib = [1]
+    if(number < 1){
+        return "OOPS"
+    }
 
+    for(i = 1; i < parseInt(number) - 1; i++){
+        if(i == 1){
+            fib.push(1)
+        }
+        fib.push(fib[i] + fib[i - 1])
+    }
+    if(number == 2 || number == 1){
+        return 1;
+    }
+
+    return fib[i]
 };
 
 // Do not edit below this line
