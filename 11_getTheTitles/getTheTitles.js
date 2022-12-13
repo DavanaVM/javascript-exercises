e497291d5e58981bf79277b883aa7daee5b5ba51
DavanaVM <gotfriddavid@gmx.de>
2022-12-13 11:47:20 +0100

    happy

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,21 @@
 const getTheTitles = function() {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
 
+      titles = [];
+      for(let i = 0; i<= books.length - 1; i++){
+        titles.push(books[i]["title"])
+      }
+      
+      return titles
 };
 
 // Do not edit below this line
